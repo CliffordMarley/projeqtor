2a67be9d2a893a2bb4468d8fb58d52e35d65b1da
root <root@vps.server.local>
2023-05-09 02:18:28 +0000

    first commit

---
+++ files/config/parameters.php
@@ -0,0 +1,19 @@
+<?php
+// =======================================================================================
+// Automatically generated parameter file
+// on 2023-04-13 06:27:34
+// =======================================================================================
+$paramDbType='mysql';
+$paramDbHost='127.0.0.1';
+$paramDbPort='3306';
+$paramDbUser='webmaster';
+$paramDbPassword='D3v@Space';
+$paramDbName='pms_db';
+$paramDbPrefix='';
+$SslKey='';
+$SslCert='';
+$SslCa='';
+$logFile='../files/logs/projeqtor_${date}.log';
+$logLevel='2';
+$enforceUTF8='1';
+//======= END
